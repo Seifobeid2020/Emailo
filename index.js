3659a0619abd25b6f4169dbf633569a2f93a7043
seifobeid2020 <201706483@bethlehem.edu>
2020-09-12 13:58:13 +0300

    first change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   // res.render("/home");
-  res.send({ hi: "there" });
+  res.send({ hi: "thessre" });
 });
 
 const PORT = process.env.PORT || 3000;

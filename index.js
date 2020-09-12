af20dee1ee3eb3f6d4a975046f8a353da1b3b6ad
seifobeid2020 <201706483@bethlehem.edu>
2020-09-12 13:11:44 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+// let template = ejs.compile(str, options);
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("/home");
+  // res.send({ hi: "there" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000);

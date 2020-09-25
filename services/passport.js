04d872e5fac62aa1f5bd1d635f58ce7e8575ec1f
seifobeid2020 <201706483@bethlehem.edu>
2020-09-25 23:23:06 +0300

    keys7

--- services/passport.js
+++ services/passport.js
@@ -22,6 +22,7 @@ export default () => {
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: "/auth/google/callback",
+        proxy: true,
       },
       (accessToken, refreshToken, profile, done) => {
         User.findOne({ googleId: profile.id }).then((existingUser) => {

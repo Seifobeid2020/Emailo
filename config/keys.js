f7f639d489a87979522a8a4ddce90f79608d9831
seifobeid2020 <201706483@bethlehem.edu>
2020-09-30 07:19:11 +0300

    fixing versions

--- config/keys.js
+++ config/keys.js
@@ -1,46 +1,8 @@
-// const keys = {
-//   googleClientID:
-//     "670818577570-eddjabghnkkabb3f3f4ebud7mn36u8qd.apps.googleusercontent.com",
-//   googleClientSecret: "YiHY26kNR0iaRGQ1SEQR4xXh",
-//   mongoURI:
-//     "mongodb+srv://dbEmail:xQgZhInj9AMpsmTm@semail.eznle.mongodb.net/test?retryWrites=true&w=majority",
-
-//   cookieKey: "redacted",
-// };
-
-// export default keys;
-
-//keys.js - figure out what set of credentials to return
-
-// if (process.env.NODE_ENV !== "production") {
-//   //We are in production - return the prod set of keys
-//   // keys = require("./prod");
-//   module.exports = require("./prod.js");
-// } else {
-//   //we are in development - return the dev keys!!!
-//   // keys = require("./dev");
-//   module.exports = require("./dev.js");
-// }
-// console.log(require("./dev.js"));
-// export const key = import("./dev.js");
-// export const key = import("./dev.js");
-// import { createRequire } from "module";
-// const require = createRequire(import.meta.url);
-
-// if (process.env.NODE_ENV !== "production") {
-//   //We are in production - return the prod set of keys
-//   // keys = require("./prod");
-//   module.exports = require("./prod.js");
-// } else {
-//   //we are in development - return the dev keys!!!
-//   // keys = require("./dev");
-//   module.exports = require("./dev.js");
-// }
 import Prodkeys from "./prod.js";
 // import Devkeys from "./dev.js";
 const f = () => {
   let keys;
-  console.log("this is process.env.NODE_ENV : " + process.env.NODE_ENV);
+
   if (process.env.NODE_ENV !== "production") {
     //We are in production - return the prod set of keys
 
@@ -53,6 +15,3 @@ const f = () => {
 
 const keys = f();
 export default keys;
-
-// import { Devkeys } from "./dev.js";
-// export default Devkeys;

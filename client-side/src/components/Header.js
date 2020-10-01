cd959800ff63d6f99dcf8f437e7ae8bb7aa5e119
seifobeid2020 <201706483@bethlehem.edu>
2020-10-01 11:59:21 +0300

    pray

--- client-side/src/components/Header.js
+++ client-side/src/components/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Payments from "./Payments";
+import { Navbar, Form, Nav, NavDropdown } from "react-bootstrap";
 class Header extends Component {
   constructor(props) {
     super(props);
@@ -12,21 +13,17 @@ class Header extends Component {
       case null:
         return;
       case false:
-        return (
-          <li>
-            <a href="/auth/google"> Login with Google </a>
-          </li>
-        );
+        return <Nav.Link href="/auth/google">Login with Google </Nav.Link>;
       default:
         return (
           <React.Fragment>
-            <li>
+            <Nav.Content>
+              {" "}
               <Payments />
-            </li>
-            <li>Credits: {this.props.auth.credits}</li>
-            <li>
-              <a href="/api/logout">Log Out</a>
-            </li>
+            </Nav.Content>
+            <Nav.Content> Credits: {this.props.auth.credits}</Nav.Content>
+
+            <Nav.Link href="/api/logout">Log Out </Nav.Link>
           </React.Fragment>
         );
     }
@@ -34,28 +31,60 @@ class Header extends Component {
   render() {
     console.log("this is props : " + JSON.stringify(this.props));
     return (
-      <nav>
-        <div className="nav-wrapper">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>
           <Link
             to={this.props.auth ? "/surveys" : "/"}
             className="left brand-logo"
           >
             SEmail
           </Link>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            {/* <li>
-              <a href="sass.html">Sass</a>
-            </li>
-            <li>
-              <a href="badges.html">Components</a>
-            </li>
-            <li>
-              <a href="collapsible.html">JavaScript</a>
-            </li> */}
+        </Navbar.Brand>
+        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
             {this.renderContent()}
-          </ul>
-        </div>
-      </nav>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+          {/* <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form> */}
+        </Navbar.Collapse>
+      </Navbar>
+
+      // <nav>
+      //   <div className="nav-wrapper">
+      //     <Link
+      //       to={this.props.auth ? "/surveys" : "/"}
+      //       className="left brand-logo"
+      //     >
+      //       SEmail
+      //     </Link>
+      //     <ul id="nav-mobile" className="right hide-on-med-and-down">
+      //       {/* <li>
+      //         <a href="sass.html">Sass</a>
+      //       </li>
+      //       <li>
+      //         <a href="badges.html">Components</a>
+      //       </li>
+      //       <li>
+      //         <a href="collapsible.html">JavaScript</a>
+      //       </li> */}
+      //       {this.renderContent()}
+      //     </ul>
+      //   </div>
+      // </nav>
     );
   }
 }
